85dc0ef17087f0e08d37a998168d0d93509c0965
trome23 <romennoodles23@yahoo.com>
2019-03-04 02:47:42 -0700

    final

--- assets/simpsons.js
+++ assets/simpsons.js
@@ -10,6 +10,62 @@ console.log(computerGuess);
 //Grabs the randCompNumber class and adds text to the page
 $("#randomCompNumber").html("Random Number: " + computerGuess);
 
-var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//variables that generate random #'s for each of the Simpson Characters
+var simp1 = Math.floor(Math.random() * 12) + 1;
+var simp2 = Math.floor(Math.random() * 12) + 1;
+var simp3 = Math.floor(Math.random() * 12) + 1;
+var simp4 = Math.floor(Math.random() * 12) + 1;
+
+//Reset function to call when the game is over and needs to be reset to the start
+function reset() {
+    userNumber = 0;
+    $("#your-number").html("Your Number: " + userNumber);
+    var computerGuess = Math.floor(Math.random() * (120 - 19)) + 19;
+    $("#randomCompNumber").html("Random Number: " + computerGuess);
+    simp1 = Math.floor(Math.random() * 12) + 1;
+    simp2 = Math.floor(Math.random() * 12) + 1;
+    simp3 = Math.floor(Math.random() * 12) + 1;
+    simp4 = Math.floor(Math.random() * 12) + 1;
+}
+
+//Game logic with if/else statement to find out if the use has to keep playing or has won/lost
+function gameStart() {
+    if (userNumber === computerGuess) {
+        wins++;
+        $("#wins").html("Wins: " + wins);
+        alert("Great job, YOU WIN!");
+        reset();
+    } 
+    else if (userNumber > computerGuess) {
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        alert("Try again, YOU LOSE!");
+        reset();
+    }
+}
+
+$("#simp1").click(function() {
+    userNumber += simp1;
+    $("#your-number").html("Your Number: " + userNumber);
+    gameStart();
+})
+
+$("#simp2").click(function() {
+    userNumber += simp2;
+    $("#your-number").html("Your Number: " + userNumber);
+    gameStart();
+})
+
+$("#simp3").click(function() {
+    userNumber += simp3;
+    $("#your-number").html("Your Number: " + userNumber);
+    gameStart();
+})
+
+$("#simp4").click(function() {
+    userNumber += simp4;
+    $("#your-number").html("Your Number: " + userNumber);
+    gameStart();
+})
 
 
